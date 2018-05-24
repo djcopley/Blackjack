8dc74525ca5b50a55ed9025c08ce8b934ee00863
Daniel Copley <djcopley@users.noreply.github.com>
2018-05-23 23:52:38 -0700

    Updated travis.yml, added unit tests

---
+++ tests/test_blackjack.py
@@ -0,0 +1,13 @@
+import unittest
+import blackjack
+
+
+class TestBlackjack(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_blackjack(self):
+        pass
